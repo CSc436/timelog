d1c8cdeebd5d1931ee880a618073be456e4b9a81
Zuoming Shi <accountistaken@gmail.com>
2014-02-09 14:16:32 -0700

    Added new table

---
+++ app/database/migrations/2014_02_09_205931_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('firstname');
+			$table->string('lastname');
+			$table->string('email')->unique();
+			$table->string('username')->unique();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('user', function(Blueprint $table)
+		{
+			Schema::dropIfExists("user");
+		});
+	}
+
+}
