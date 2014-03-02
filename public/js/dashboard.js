a073e2444a85d3562f2e9cc1c70a4d91a113c619
Gopal Adhikari <gopala@email.arizona.edu>
2014-03-02 12:08:45 -0700

    Added skeleton structure for the dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,12 +4,12 @@ $(function() {
 
 	Dashboard.getWeather = function() {
 		$.ajax({
-			url: "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
+			url: "http://api.wunderground.com/api/d513969ef432e427/geolookup/conditions/q/IA/Cedar_Rapids.json",
 			dataType: "jsonp",
 			success: function(parsed_json) {
 				var location = parsed_json['location']['city'];
 				var temp_f = parsed_json['current_observation']['temp_f'];
-				
+
 				$("#weather").html();
 			}
 		});
