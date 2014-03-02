19a292c36074be3666d4c00c80fe304a9782bc1e
Michael_Knatz <michaelcknatz@email.arizona.edu>
2014-03-02 12:50:57 -0700

    added dashboard to navigation bar, and base dashboard layout is up

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -1,10 +1,23 @@
 @extends('layout')
-
+<!-- weather API Key: d513969ef432e427 -->
 @section('content')
 	<h2 class="title">Dashboard</h2>
 
 	<div id="dashboard-container">
-		<div id="dashboard-left-sidebar">
+		<div id="dashboard-left-sidebar" class="left-panel" 
+		style="width:200px;height:800px;padding:10px;" >
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<h4 class="panel-title">Quick Links</h4>
+					</div>
+				<div class="panel-body">
+					<p><tr><strong>Create Tasks</strong></tr> </p>
+					<p><tr><strong>View Tasks</strong></tr> </p>
+					<p><tr><strong>Completed Tasks</strong></tr> </p>
+					<p><tr><strong>View Data</strong></tr> </p>
+					<p><tr><strong>Achievements</strong></tr> </p>
+				</div>
+				</div>
 		</div>
 		<div id="weather"></div>
 		<div id="categories"></div>
