ef2cf5fa06a45e1477560d59199d5c8c6f539f09
Gopal Adhikari <gopala@email.arizona.edu>
2014-03-02 15:09:59 -0700

    Dashboard now displays time, date and weather conditions

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -3,49 +3,57 @@
 @section('content')
 	<h2 class="title">Dashboard</h2>
 
-	<div id="dashboard-container">
-		<div id="dashboard-left-sidebar" class="pull-left">
+	<div id="dashboard-container" class="row">
+
+		<!-- Left sidebar column -->
+		<div id="dashboard-left-sidebar" class="col-md-2 pull-left">
 				<div class="panel panel-info">
 					<div class="panel-heading">
 						<h3 class="panel-title">Quick Links</h4>
 					</div>
 					<div class="panel-body">
-						<p><tr><strong>Create Tasks</strong></tr> </p>
-						<p><tr><strong>View Tasks</strong></tr> </p>
-						<p><tr><strong>Completed Tasks</strong></tr> </p>
-						<p><tr><strong>View Data</strong></tr> </p>
-						<p><tr><strong>Achievements</strong></tr> </p>
+						<p>View Tasks</p>
+						<p>Completed Tasks</p>
+						<p>View Data</p>
+						<p>Achievements</p>
+						<p>Create Tasks</p>
 					</div>
 				</div>
 		</div>
 
-		<div id="weather">
+		<!-- Weather column -->
+		<div id="weather" class="box col-md-2">
 			<div id="weather-condition"></div>
 			<div id="temperature"></div>
 			<div id="location"></div>
 			<div id="time"></div>
+			<div id="date"></div>
 		</div>
 		
-		<div id="categories"></div>
+		<!-- Categories column -->
+		<div id="categories" class="box col-md-5"></div>
 		
-		<div id="trends"></div>
+		<!-- Trends column -->
+		<div id="trends" class="box col-md-6">
+			
+		</div>
 		
-		
-		<div id="dashboard-right-sidebar" class="pull-right" >
+		<!-- Right sidebar column -->
+		<div id="dashboard-right-sidebar" class="col-md-2 pull-right" >
 			<div id="deadlines">
-				<h3 class="panel- title">Deadlines</h3>
+				<h3 class="panel-title">Deadlines</h3>
 				<div class="panel-body">
 					
 				</div>
 			</div>
 			<div id="achievements">
-				<h4 class="panel- title">Achievements</h4>
+				<h4 class="panel-title">Achievements</h4>
 				<div class="panel-body">
-					Hello
+					Congrats! You are the most productive person in the world.
 				</div>
-			</div>
-				
+			</div>				
 		</div>
+
 	</div>
 
 	<link href="{{ URL::asset('css/dashboard.css') }}" rel="stylesheet">
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -10,19 +10,31 @@ $(function() {
 				var location = parsed_json['location']['city'];
 				var temp_f = parsed_json['current_observation']['temp_f'];
 				
-				$("#temparature").text(temp_f);
-				$("#weather-condition").text(temp_f);
+				$("#temperature").html(temp_f + " &deg;F");
+				
+				var icon_url = parsed_json['current_observation'].icon_url;
+				var icon = $("<img>", { src: icon_url});
+				$("#weather-condition").append(icon);
 				$("#location").text(location);
+
+				console.log(parsed_json);
+
 			}
 		});
 	}
 
 	Dashboard.getTime = function(){
 		$("#time").text(moment().format("hh:mma"));
+		$("#date").text(moment().format("ddd / MMMM DD / YYYY"));
+	}
+
+	Dashboard.getDeadline = function(){
+		
 	}
 
 	Dashboard.getWeather();
 	Dashboard.getTime();
+	Dashboard.getDeadline();
 
 	window.setInterval(function(){
 		Dashboard.getWeather();
