3bf8d14429b642050e68af313f53e9c5e91aa8d1
monkey300012 <victornguyen@email.arizona.edu>
2014-03-30 11:23:33 -0700

    current status

--- app/api_routes.php
+++ app/api_routes.php
@@ -14,23 +14,16 @@ Route::group(array('prefix' => 'api', 'before' => 'auth'), function(){
 		return $data;
 	});
 
-	Route::get('log/view', function()
-	{
-		// $data = DB::table('log_entry')
-		// 					->select("DATE_FORMAT(startDateTime,'%m-%d-%y') as label")
-		// 					->select("duration as value")
-		// 					->where('cid', '=', 10)
-		// 					->orderBy('label', 'asc')
-		// 					->orderBy('duration', 'desc')
-		// 					->get();
-		$data = DB::select("select DATE_FORMAT(startDateTime,'%m-%d-%y') as label, duration as value from log_entry where cid = 10 order by label asc, duration desc");
-		return $data;
-	});
-
 	Route::get('log/pie', function()
 	{
 		// select name, color, duration from log_entry e, log_category c where c.cid = e.cid and c.uid = e.uid
 		$data = DB::select("select name as label, CAST(SUM(duration) as unsigned) as value , color as color from log_category c, log_entry e where c.cid = e.cid group by label");
+		return $data;
+	});
+
+	Route::get('log/data', function()
+	{ 
+		$data = DB::select("select DATE_FORMAT(startDateTime,'%m-%d-%y') as label, duration as value from log_entry order by label asc, duration desc");
 		return $data;
 	});
 });
