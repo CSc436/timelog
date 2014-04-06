ea0575aec3cc50e910c11378c55dc8aa4c39f609
james <yoshida@email.arizona.edu>
2014-04-06 14:58:58 -0700

    subcategory view with jscript functional

--- app/views/viewCategories.blade.php
+++ app/views/viewCategories.blade.php
@@ -3,6 +3,18 @@
 @section('content')	
 	<link href="{{ URL::asset('js/sorttable.js') }}" rel="text/javascript">
 	<script src="{{ URL::asset('js/sorttable.js') }}"></script>
+	<script type = "text/javascript">
+		function viewSubcat(cid) {
+			console.log(cid);
+			var item = document.getElementsByClassName(cid)[0];
+			console.log(item);
+			if(item) {
+				$("."+cid).toggle('2000', 'swing', function(){
+
+				});
+			}
+		}
+	</script>
 	<div class="container" id="main">
 
 	<h2 class="title">Categories</h2>
@@ -13,7 +25,7 @@
 			{
 				if ($entries->isTask == 0){
 					if ($entries->PID == NULL){
-						echo ("<li>".$entries->name. "<button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/category".$entries->CID."/modal'})\">Edit</button></li>");
+						echo ("<li onclick = viewSubcat($entries->CID)>".$entries->name. /*"<button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/category".$entries->CID."/modal'})\">Edit</button>*/"</li>");
 						subCategories($entries);
 					}
 				}
@@ -21,10 +33,10 @@
 
 			function subCategories($parentCategory) {
 		 	$id = Auth::user()->id;
-		 	echo("<ul>");
+		 	echo("<ul class = 'subcat'>");
 				$subCats = DB::select("select * from log_category c where c.pid =  $parentCategory->CID");
 				foreach($subCats as $sub){
-					echo ("<li>" . $sub->name . "<button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/category".$sub->CID."/modal'})\">Edit</button></li>");
+					echo ("<li class = 'subcat $parentCategory->CID' onclick = viewSubcat($sub->CID) style = 'display:none'>" . $sub->name . /*"<button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/category".$sub->CID."/modal'})\">Edit</button>*/"</li>");
 					subCategories($sub);
 			 	}
 		 	echo("</ul>");
