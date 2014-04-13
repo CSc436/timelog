f3d43be1e598434abc7246d76d61561cd4bb09f3
Zuoming <Zuoming@dhcp-10-134-203-254.uawifi.arizona.edu>
2014-04-13 16:51:40 -0700

    Calendar displays catagory colors

--- public/js/addlog_cal.js
+++ public/js/addlog_cal.js
@@ -7,22 +7,46 @@ $(document).ready(function() {
 	var m = date.getMonth();
 	var y = date.getFullYear();
 	
+	var categories = [];
+
+	$.get( "/log/view_cat_cal", function(cats) {
+		//TODO: do selective querying when real dates are used, so we don't have to
+		//get all events, however, it is unclear yet how much of a performance bottleneck
+		//this will be.
+		for ( var i=0; i<cats.length; i++ ) {
+			categories.push({
+				title : cats[i].name,
+				id : cats[i].CID,
+				color : cats[i].color
+			});
+		}
+	});
+
 	var eventsBeforeParsed = [];
 	var eventsAfterParsed = [];
 	$.get( "/log/view_cal", function(events) {
+		console.log(categories);
 		eventsBeforeParsed = events;
 		console.log(events);
 		//TODO: do selective querying when real dates are used, so we don't have to
 		//get all events, however, it is unclear yet how much of a performance bottleneck
 		//this will be.
 		for ( var i=0; i<events.length; i++ ) {
+			events[i].color = "#FFFFFF";
+			for(var i2=0; i2<categories.length; i2++){
+				if(events[i].CID == categories[i2].id){
+					events[i].color = "#" + categories[i2].color;
+				}
+			}
+
 			eventsAfterParsed.push({
 				title : "placeholder title",
 				start: new Date(events[i].startDateTime),
 				end: new Date(events[i].endDateTime),
 				allDay: false,
 				description: events[i].notes,
-				id : events[i].LID
+				id : events[i].LID,
+				backgroundColor : events[i].color
 			});
 		}
 	});
