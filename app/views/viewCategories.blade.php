52f8d69e1e084b0c44ec9e297bd365751bfac8a9
Michael_Knatz <michaelcknatz@email.arizona.edu>
2014-04-13 12:48:07 -0700

    collapsible list added for css with images

--- app/views/viewCategories.blade.php
+++ app/views/viewCategories.blade.php
@@ -1,43 +1,42 @@
 @extends('layout')
-
-@section('content')	
-	<link href="{{ URL::asset('js/sorttable.js') }}" rel="text/javascript">
-	<script src="{{ URL::asset('js/sorttable.js') }}"></script>
+@section('header')
+	<link href="{{ URL::asset('css/jquery.collapsibleList.css') }}" rel="stylesheet"/>
+	<script src="{{ URL::asset('js/jquery.collapsibleList.js') }}"></script>
+@stop
+@section('content')
+	
 	<script type = "text/javascript">
-		function viewSubcat(cid) {
-			console.log(cid);
-			var item = document.getElementsByClassName(cid)[0];
-			console.log(item);
-			if(item) {
-				$("."+cid).toggle('2000', 'swing', function(){
-
-				});
-			}
-		}
+		$(function(){
+		$(".collapsible").collapsibleList();
+		});
 	</script>
 	<div class="container" id="main">
 
 	<h2 class="title">Categories</h2>
 	<div name="category" id="category">
-	<ul class="list sortable" >
+	<ul class="collapsible" >
 		<?php
-			foreach ($categories as $entries)
+			foreach ($categories as $entry)
 			{
-				if ($entries->isTask == 0){
-					if ($entries->PID == NULL){
-						echo ("<li onclick = viewSubcat($entries->CID)>".$entries->name. /*"<button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/category".$entries->CID."/modal'})\">Edit</button>*/"</li>");
-						subCategories($entries);
+				if ($entry->isTask == 0){
+					if ($entry->PID == NULL){
+						echo ("<li>".$entry->name);
+						subCategories($entry);
+						echo("</li>");
 					}
 				}
 			}
 
 			function subCategories($parentCategory) {
 		 	$id = Auth::user()->id;
-		 	echo("<ul class = 'subcat'>");
+		 	echo("<ul>");
 				$subCats = DB::select("select * from log_category c where c.pid =  $parentCategory->CID");
 				foreach($subCats as $sub){
-					echo ("<li class = 'subcat $parentCategory->CID' onclick = viewSubcat($sub->CID) style = 'display:none'>" . $sub->name . /*"<button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/category".$sub->CID."/modal'})\">Edit</button>*/"</li>");
+					/*"<button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/category".$sub->CID."/modal'})\">Edit</button>*/
+					echo ("<li>" . $sub->name);
 					subCategories($sub);
+					echo("</li>");
+					
 			 	}
 		 	echo("</ul>");
 		}
@@ -55,20 +54,20 @@
 				<th> Grade    </th>
 			</tr>
 			<?php
-				foreach ($categories as $entries)
+				foreach ($categories as $entry)
 				{
-					if ($entries->isTask == 1) {
-						echo ("<tr><td>".$entries->name."</td>");
-						echo ("<td>". $entries->deadline ."</td>");
-						if ($entries->isCompleted == 1){
+					if ($entry->isTask == 1) {
+						echo ("<tr><td>".$entry->name."</td>");
+						echo ("<td>". $entry->deadline ."</td>");
+						if ($entry->isCompleted == 1){
 							echo ("<td> yes </td>");
-							echo ("<td>". $entries->rating ."</td>");
+							echo ("<td>". $entry->rating ."</td>");
 						}
 						else {
 							echo ("<td> no </td>");
 							echo ("<td> N/A </td>");
 						}
-						echo ("<td></td><td><button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/".$entries->CID."/modal'})\">Edit</button></td></tr>");
+						echo ("<td></td><td><button class=\"btn btn-xs\" onclick=\"return $('#thisModal').modal({remote: '/log/edit/".$entry->CID."/modal'})\">Edit</button></td></tr>");
 					}
 				}
 			?>
